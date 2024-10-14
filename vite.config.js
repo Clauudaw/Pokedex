883dfee329860581f8c447dba04eb1f5ffec3c5a
Claudia Rey <claudiardaw@gmail.com>
2024-10-14 09:31:42 +0200

    feat: sass

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { ref } from "vue";
 import { defineConfig } from 'vite'
+
+import { usePokemon } from "./composables/usePokemon";
+import searchBar from "./components/SearchBar.vue";
+
 import vue from '@vitejs/plugin-vue'
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
